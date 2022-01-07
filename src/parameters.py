7ff93afba8d350a62062dfea7963a4a737c48e9f
michgz <mdr.nz11@gmail.com>
2022-01-07 23:35:21 +0000

    A file to be overwritten by the pre-processing step

---
+++ src/parameters.py
@@ -0,0 +1,7 @@
+## This file is to be automatically generated.
+
+
+raise Exception("""Incorrectly imported file parameters.py. This file should be
+                   generated before running the program. Do this now by running
+                   casio\pre_process.py if you have not already done so.""")
+
