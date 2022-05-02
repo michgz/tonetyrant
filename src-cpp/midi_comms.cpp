03c6c72ef3b1df7413027000f2827d89249fe9ef
michgz <mdr.nz11@gmail.com>
2022-05-02 19:10:09 +0100

    More on midi

--- src-cpp/midi_comms.cpp
+++ src-cpp/midi_comms.cpp
@@ -6,6 +6,11 @@
 #include <vector>
 
 
+#define wxTrue true
+#ifndef wxFalse
+#define wxFalse false
+#endif
+
 
 std::vector<unsigned char> midi_7bit_to_8bit(std::vector<unsigned char> b)
 {
@@ -146,5 +151,203 @@ std::vector<unsigned char> midi_8bit_to_7bit(std::vector<unsigned char> b)
 
 
 
+   /* Define the device ID. Constructed as follows:
+    #    0x44       Manufacturer ID ( = Casio)
+    #    0x19 0x01  Model ID ( = CT-X3000, CT-X5000, CT-X700)
+    #    0x7F       Device. This is a "don't care" value
+    #*/
+const std::vector<unsigned char> DEVICE_ID = {0x44, 0x19, 0x01, 0x7F};
+    
+    
+std::vector<unsigned char> make_packet(bool tx,
+                                        std::vector<unsigned char> data,
+                                        unsigned short int category=30,
+                                        unsigned short int memory=1,
+                                        unsigned short int parameter_set=0,
+                                        unsigned short int block[4] = (unsigned short int[]){0,0,0,0},
+                                        unsigned short int parameter=0,
+                                        unsigned char      index=0,
+                                        int                length=1,
+                                        int                command=-1,
+                                        int                sub_command=3)
+{
+    /*
+    Construct a packet in Casio SYSEX format, for sending over the MIDI port
+    */
+      
+    std::vector<unsigned char> w;
 
+    w.push_back(0xF0);
+    w.push_back(DEVICE_ID[0]);
+    w.push_back(DEVICE_ID[1]);
+    w.push_back(DEVICE_ID[2]);
+    w.push_back(DEVICE_ID[3]);
+
+
+    if (command < 0)
+    {
+        if (tx)
+        {
+            command = 1;
+        }
+        else
+        {
+            command = 0;
+        }
+    }
+    w.push_back((unsigned char) command);
+
+    if (command == 0x8)
+    {
+        w.push_back((unsigned char) sub_command);
+        w.push_back(0xF7);
+        return w;
+    }
+
+    w.push_back((unsigned char) category);
+    w.push_back((unsigned char) memory);
+    w.push_back((unsigned char) (parameter_set & 0x007F));
+    w.push_back((unsigned char) (parameter_set/128));
+
+    if (command == 0xA)
+    {
+        w.push_back(0xF7);
+        return w;
+    }
+    else if (command == 5)
+    {
+        w.push_back((unsigned char) (length & 0x007F));
+        w.push_back((unsigned char) (length/128));
+        //w += self.midi_8bit_to_7bit(data)
+        //crc_val = binascii.crc32(w[1:])
+        //w += self.midi_8bit_to_7bit(struct.pack('<I', crc_val))
+        w.push_back(0xF7);
+        return w;
+    }
+    else if ((command >= 2 && command < 8) || command == 0xD || command == 0xE) // OBR/HBR doesn't have the following stuff
+    {
+    }
+    else
+    {
+        // Blocks are done in reverse order. This means that block[0] is block0, etc.
+        w.push_back((unsigned char) (block[3] & 0x007F));
+        w.push_back((unsigned char) (block[3]/128));
+        w.push_back((unsigned char) (block[2] & 0x007F));
+        w.push_back((unsigned char) (block[2]/128));
+        w.push_back((unsigned char) (block[1] & 0x007F));
+        w.push_back((unsigned char) (block[1]/128));
+        w.push_back((unsigned char) (block[0] & 0x007F));
+        w.push_back((unsigned char) (block[0]/128));
+        w.push_back((unsigned char) (parameter & 0x007F));
+        w.push_back((unsigned char) (parameter/128));
+        w.push_back((unsigned char) (index));
+        w.push_back((unsigned char) (length-1));
+    }
+    if (tx)
+    {
+        for (auto D = data.begin(); D != data.end(); D++)
+        {
+            w.push_back(*D);
+        }
+    }
+    w.push_back(0xF7);
+    return w;
+
+}
+
+
+
+void set_single_parameter(int parameter, 
+                        unsigned long int data, 
+                        int midi_bytes=1, 
+                        int category=3, 
+                        int memory=3, 
+                        int parameter_set=0, 
+                        int block0=0, 
+                        int block1=0, 
+                        bool _debug=wxFalse)
+{
+    /*
+    Send a single parameter value to the keyboard
+    */
+
+    //_logger = logging.getLogger()
+    //_logger.info(f" parameter {parameter}, block {block0} <- {str(data)}:")
+
+
+
+
+    // Open the device (if needed)
+    /*   midiin = rtmidi.MidiIn()
+    midiout = rtmidi.MidiOut()
+    for i in range(midiout.get_port_count()):
+      if self._output_name == midiout.get_port_name(i):
+          midiout.open_port(port=i)
+    for i in range(midiin.get_port_count()):
+      if self._input_name == midiin.get_port_name(i):
+          midiin.open_port(port=i)
+    if not midiout.is_port_open() or not midiin.is_port_open():
+      raise Exception("Could not find the named port")
+
+    midiin.ignore_types(sysex=False)*/
+
+
+
+    // Flush the input queue
+    /* time.sleep(0.01)
+    midiin.get_message()*/
+
+    // Prepare the input
+    std::vector<unsigned char> d;
+    int l = 1;
+
+    //if isinstance(data, type(0)):
+    if (true)
+    {
+        /* The input is an integer. The "length" parameter passed to make_packet must be
+        # 1, but we don't know how many bytes of bit-stuffed data the keyboard is actually
+        # expecting. Use the "midi_bytes" parameter for that.*/
+
+        int key_len = midi_bytes;
+        
+        // Now do the bit-stuffing
+        for (int i = 0; i < key_len; i ++)
+        {
+            d.push_back((unsigned char) data&0x7F);
+            data = data/0x80;
+        }
+        l = 1;   // length is always 1 for numeric inputs
+    }
+    else
+    {
+        // Assume the input is a byte array
+      //  d = data
+      //  l = len(d)
+    }
+      
+    // Write the parameter
+    unsigned short int blocks[4] = {block0,block1, 0, 0};
+    auto pkt = make_packet(wxTrue, d, category, memory, parameter_set, blocks, parameter, 0, 1);
+ //   midiout.send_message(bytearray(pkt))
+
+#if 0
+    _logger.info("    " + pkt.hex(" ").upper())
+    time.sleep(0.1)
+    // Handle any response -- don't expect one
+    midiin.get_message()
+    time.sleep(0.01)
+
+    // Close the device
+    midiin.close_port()
+    midiout.close_port()
+
+    /* Also delete the instances. See notes in rtmidi-python documentation. This is
+    # needed to get around delays in the python garbage-collector. A better
+    # solution might be to keep the ports open and close only when exiting the
+    # program.*/
+    midiin.delete()
+    midiout.delete()
+#endif
+
+}
 
